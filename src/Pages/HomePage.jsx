d097c44c1257c9b34b61a864e7268d0c8b6bb08a
theo geneste <theo.boxe@hotmail.fr>
2024-11-06 12:30:34 +0100

    Filtering data

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,15 +1,23 @@
 import { useEffect, useState } from "react";
 import ChampionService from "../Services/ChampionService";
 import ChampionCard from "../Components/ChampionCard";
-import { Container } from "react-bootstrap";
+import { Container, Form } from "react-bootstrap";
 
 const HomePage = () => {
-    const [champions, setChampions] = useState({});
+    const [champions, setChampions] = useState([]);
+    const [searchValue, setSearchValue] = useState("");
+    const [filteredChampions, setFilteredChampions] = useState([])
+
+    const handleChange = (e) => {
+        setSearchValue(e.currentTarget.value);
+    }
 
     const fetchChampions = async () => {
         try {
             const response = await ChampionService.getAllChampions();
-            setChampions(response.data.data);
+            const res = Object.entries(response.data.data);
+            setChampions(res);
+            setFilteredChampions(res);
         } catch (error) {
             console.log(error);
         }
@@ -19,10 +27,24 @@ const HomePage = () => {
         fetchChampions();
     }, [])
 
+    useEffect(() => {
+        setFilteredChampions(champions.filter((champion) => {
+            return champion[1].name.toLowerCase().includes(searchValue.toLowerCase());
+            // return champion[1].name.toLowerCase().startsWith(searchValue.toLowerCase());
+        }))
+    }, [searchValue])
+
+
     return <Container className="d-flex flex-column align-items-center">
         <h1>Champions</h1>
+        <Form className="col-12">
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Form.Label>Recherche ton champion</Form.Label>
+                <Form.Control type="text" placeholder="Exemple : Aatrox" value={searchValue} onChange={handleChange}/>
+            </Form.Group>
+        </Form>
         <div className="d-flex justify-content-center gap-3 flex-wrap">
-           {Object.entries(champions).map((champion) => {
+           {filteredChampions.map((champion) => {
                 return <ChampionCard  championCard={champion[1]} key={champion[1].id}/>
             })} 
         </div>
--- src/Pages/ItemsPage.jsx
+++ src/Pages/ItemsPage.jsx
@@ -1,15 +1,28 @@
-import { Container } from "react-bootstrap";
+import { Container, Form } from "react-bootstrap";
 import ItemService from "../Services/ItemService";
 import { useEffect, useState } from "react";
 import ItemCard from "../Components/ItemCard";
 
 const ItemsPage = () => {
-    const [items, setItems] = useState({});
+    const [items, setItems] = useState([]);
+    const [searchValue, setSearchValue] = useState("");
+    const [filteredItems, setFilteredItems] = useState([])
+
+    const handleChange = (event) =>{
+        setSearchValue(event.currentTarget.value);   
+    }
 
     const fetchItems = async () => {
         try {
             const response = await ItemService.getAllItems();
-            setItems(response.data.data);
+            
+            const res =  Object.entries(response.data.data);
+            res.sort((a,b) => {
+               return a[1].name.localeCompare(b[1].name);
+            })
+            // response.data.data.sort((a, b) => a.name.localeCompare(b.firstname))
+            setItems(res);
+            setFilteredItems(res);
         } catch (error) {
             console.log(error);
         }
@@ -19,10 +32,23 @@ const ItemsPage = () => {
         fetchItems();
     }, [])
 
+    useEffect(()=>{
+        setFilteredItems(items.filter((item) => {
+            return item[1].name.toLowerCase().includes(searchValue.toLowerCase());
+            // return item[1].name.toLowerCase().startsWith(searchValue.toLowerCase());
+        }))
+    }, [searchValue])
+
     return  <Container className="d-flex flex-column align-items-center">
         <h1>Items :</h1>
+        <Form className="col-12">
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Form.Label>Recherche ton Item</Form.Label>
+                <Form.Control type="text" placeholder="Exemple : Bottes" value={searchValue} onChange={handleChange}/>
+            </Form.Group>
+        </Form>
         <div className="d-flex flex-wrap gap-5">
-            {Object.entries(items).map((item) => {
+            {filteredItems.map((item) => {
                 return <ItemCard itemCard={item[1]} key={item[0]} />
             })}
         </div>
